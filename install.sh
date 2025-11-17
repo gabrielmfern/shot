9435113a205f64ec2d0593fe749a1854e09f8552
Gabriel Miranda <gabrielmfern@outlook.com>
2025-11-17 16:55:52 -0300

    fix install.sh on MacOS

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 zig build install
-sudo cp --force ./zig-out/bin/shot /usr/bin/shot-raw
+sudo cp ./zig-out/bin/shot /usr/local/bin/shot-tui
 
 if [ -f "/usr/bin/shot" ]; then
-  sudo rm -f /usr/bin/shot
+  sudo rm -f /usr/local/bin/shot
 fi
 
-sudo touch /usr/bin/shot
-sudo tee /usr/bin/shot > /dev/null << 'WRAPPER_EOF'
+sudo touch /usr/local/bin/shot
+sudo tee /usr/local/bin/shot > /dev/null << 'WRAPPER_EOF'
 #!/bin/sh
-cmd=$(/usr/bin/shot-raw "$@" 2>/dev/tty)
+cmd=$(/usr/local/bin/shot-tui "$@" 2>/dev/tty)
 eval "$cmd"
 WRAPPER_EOF
 
-sudo chmod +x /usr/bin/shot
+sudo chmod +x /usr/local/bin/shot

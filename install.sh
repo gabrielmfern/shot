406bbcf1e6bd42668acac8365009f48e69918388
gabriel miranda <gabrielmfern@outlook.com>
2025-11-17 09:11:48 -0300

    update install.sh to work, but the cd is not sufficient

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 zig build install
-sudo cp ./zig-out/bin/shot
+sudo cp --force ./zig-out/bin/shot /usr/bin/shot-raw
+
+if [ -f "/usr/bin/shot" ]; then
+  sudo rm -f /usr/bin/shot
+fi
+
+sudo touch /usr/bin/shot
+sudo tee /usr/bin/shot > /dev/null << 'WRAPPER_EOF'
+#!/bin/sh
+cmd=$(/usr/bin/shot-raw "$@" 2>/dev/tty)
+eval "$cmd"
+WRAPPER_EOF
+
+sudo chmod +x /usr/bin/shot
